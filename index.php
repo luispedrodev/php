f77d0e4e9251b7231e78643ee5f3f492c001204e
Luis Pedro <luispedro.dev@gmail.com>
2018-07-07 15:57:46 -0300

    resolvi o conflito

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <? php
 
-<<<<<<< HEAD
+
 echo "Hello world!";
 
 echo"testando 456"	
-=======
-echo "Hello world again!";
- 
-echo "testando123"
->>>>>>> 7857b3acff7de18791021225cc1e5e180fa4a92c
+
 ?>
